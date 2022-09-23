38d95adf6716093b9fe2dd0ce88069156e940782
ilya-kolomak <99127402+ilya-kolomak@users.noreply.github.com>
2022-09-23 17:19:14 +0200

    0.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+const LOCALE_STORAGE_KEY = 'contact-form-key';
+import { save, load, remove } from './storage';
 
+initPage();
+
+const onFormInput = event => {
+  const { name, value } = event.target;
+
+  let saveData = load(LOCALE_STORAGE_KEY);
+  saveData = saveData ? saveData : {};
+
+  saveData[name] = value;
+
+  save(LOCALE_STORAGE_KEY, saveData);
+};
+
+const throttledOnFormInput = throttle(onFormInput, 500);
+formRef.addEventListener('input', throttledOnFormInput);
+
+function initPage() {
+  const saveData = load(LOCALE_STORAGE_KEY);
+
+  if (!saveData) {
+    return;
+  }
+  Object.entries(saveData).forEach(([name, value]) => {
+    formRef.elements[name].value = value;
+  });
+}
+
+const handleSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  console.log({ email: email.value, message: message.value });
+  event.currentTarget.reset();
+  remove(LOCALE_STORAGE_KEY);
+};
+
+formRef.addEventListener('submit', handleSubmit);
